1f0815c5e15da3b475d4684486e01dcb67cb1f3b
Abhi Malusare <malusareabhi1@gmail.com>
2025-07-01 23:55:16 +0530

    Update kapil3pm3.py

--- kapil3pm3.py
+++ kapil3pm3.py
@@ -272,7 +272,10 @@ filtered_breakout_df['P&L'] = breakout_df['P&L'].apply(color_pnl_text)
 
 
 filtered_breakout_df['P&L'] = breakout_df['P&L'].apply(color_pnl_text)
+st.subheader("📘 Breakout Logs")
 st.dataframe(filtered_breakout_df)
+show_trade_metrics(filtered_breakout_df, "Breakout Trades")
+#st.dataframe(filtered_breakout_df)
 
 #filtered_breakout_df['P&L'] = breakout_df['P&L'].apply(color_pnl_text)
 #st.dataframe(filtered_breakout_df)
@@ -282,9 +285,7 @@ filtered_breakout_df = breakout_df[breakout_df['Result'] != '❌ No Entry']
 #st.dataframe(filtered_breakout_df.style.applymap(color_pnl, subset=['P&L']))
 
 
-st.subheader("📘 Breakout Logs")
-st.dataframe(filtered_breakout_df)
-show_trade_metrics(filtered_breakout_df, "Breakout Trades")
+
 
 st.download_button("📥 Download Log", filtered_breakout_df.to_csv(index=False), file_name="breakout_log.csv")
 #filtered_breakout_df = filtered_breakout_df[filtered_breakout_df['Result'] != '❌ No Entry']
