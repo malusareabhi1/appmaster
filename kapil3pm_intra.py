598adf486cf43cf082482433868560ac619caf5c
Abhi Malusare <malusareabhi1@gmail.com>
2025-07-05 14:50:38 +0530

    Update kapil3pm_intra.py

--- kapil3pm_intra.py
+++ kapil3pm_intra.py
@@ -3,7 +3,7 @@ import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
-import plotly.express as px
+#import plotly.express as px
 #import plotly.express as px
 st.set_page_config(page_title="NIFTY 15-Min Chart with 3PM Breakout Strategy", layout="wide")
 
