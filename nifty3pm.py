43485a8035d846374003e3b67cdb7c5c0e70a2c5
Abhi Malusare <malusareabhi1@gmail.com>
2025-07-07 13:17:47 +0530

    Update nifty3pm.py

--- nifty3pm.py
+++ nifty3pm.py
@@ -254,7 +254,7 @@ def plot_candlestick_chart(df, df_3pm):
         fig.add_annotation(
             x=dt_start,
             y=row['open'],
-            text=f"{dt_start.strftime('%b %d')} 3PM Open",
+            text=f"{dt_start.strftime('%b %d')} ",
             showarrow=False,
             xanchor="left",
             yanchor="bottom",
@@ -272,7 +272,7 @@ def plot_candlestick_chart(df, df_3pm):
         fig.add_annotation(
             x=dt_start,
             y=row['close'],
-            text=f"{dt_start.strftime('%b %d')} 3PM Close",
+            text=f"{dt_start.strftime('%b %d')} ",
             showarrow=False,
             xanchor="left",
             yanchor="top",
